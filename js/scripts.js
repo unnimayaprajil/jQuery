5e43da2f4f0e4627b40c4b3fcbf29d39db0d908c
unnimayaprajil <unnimayasubramanyan@gmail.com>
2024-04-29 12:16:19 +0530

    effects

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,32 @@
 // }
 
 $(document).ready(()=>{
-    $('#btn1').mouseleave(()=>{
-        $('h2').fadeOut()
+    $('#btn1').click(()=>{
+        $('h2').fadeIn(3000)
+    })
+    $('#btn2').click(()=>{
+        $('h2').fadeOut(3000)
+    })
+    $('#btn3').click(()=>{
+        $('h2').fadeToggle(3000)
+    })
+    $('#btn4').click(()=>{
+        $('h2').hide(3000)
+    })
+    $('#btn5').click(()=>{
+        $('h2').show(3000)
+    })
+    $('#btn6').click(()=>{
+        $('h2').toggle(3000)
+    })
+    $('#btn7').click(()=>{
+        $('h2').slideUp(3000)
+    })
+    $('#btn8').click(()=>{
+        $('h2').slideDown(3000)
+    })
+    $('#btn9').click(()=>{
+        $('h2').slideToggle(3000)
     })
         
     
